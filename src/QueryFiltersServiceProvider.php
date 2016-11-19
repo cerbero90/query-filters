d8a282636d3940f1b5897b14b21f66d4709b4de1
Andrea Marco Sartori <22cerbero22@gmail.com>
2016-11-19 14:00:33 +1100

    Create service provider

---
+++ src/QueryFiltersServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Cerbero\QueryFilters;
+
+use Cerbero\QueryFilters\MakeQueryFiltersCommand;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * The query filters service provider.
+ *
+ * @author    Andrea Marco Sartori
+ */
+class QueryFiltersServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/query_filters.php' => config_path('query_filters.php'),
+        ], 'query_filters_config');
+
+        $this->mergeConfigFrom(
+            __DIR__.'/query_filters.php', 'query_filters'
+        );
+
+        if ($this->app->runningInConsole()) {
+            $this->commands(MakeQueryFiltersCommand::class);
+        }
+    }
+}
