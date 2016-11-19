b5b835137674116e0efd4e6a628e2f60f059481b
Andrea Marco Sartori <22cerbero22@gmail.com>
2016-11-19 14:00:05 +1100

    Create configuration

---
+++ src/query_filters.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Query Filters Path
+    |--------------------------------------------------------------------------
+    |
+    | Directory to store the query filters in. By default is app/QueryFilters.
+    */
+    'path' => 'QueryFilters',
+];
