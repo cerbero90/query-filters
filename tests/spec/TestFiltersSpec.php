878e0c52e1012dc0a8c6e1ddb3307dbd057a4064
Andrea Marco Sartori <22cerbero22@gmail.com>
2017-08-15 22:54:37 +1000

    Update tests

--- tests/spec/TestFiltersSpec.php
+++ tests/spec/TestFiltersSpec.php
@@ -27,24 +27,86 @@ class TestFiltersSpec extends ObjectBehavior
     }
 
     /**
-     * @testdox    It applies all the filters to the given query.
+     * @testdox    It applies explicit filters.
      *
      * @author    Andrea Marco Sartori
      * @return    void
      */
-    public function it_applies_all_the_filters_to_the_given_query($request, Builder $query)
+    public function it_applies_explicit_filters($request, Builder $query)
     {
         $filters = [
             'foo'         => 'ciao',
             'bar'         => '0',
             'baz'         => '',
-            'foo-bar_baz' => '',
+            'foo-bar_baz' => 'null',
+        ];
+
+        $appliedFilters = [
+            'foo'         => 'ciao',
+            'bar'         => '0',
+            'foo-bar_baz' => 'null',
         ];
 
         $request->all()->willReturn($filters);
 
         $this->applyToQuery($query)->shouldReturn($query);
 
-        $this->filters->shouldBe($filters);
+        $this->filters->shouldBe($appliedFilters);
+    }
+
+    /**
+     * @testdox    It applies registered implicit filters.
+     *
+     * @author    Andrea Marco Sartori
+     * @return    void
+     */
+    public function it_applies_registered_implicit_filters($request, Builder $query)
+    {
+        $filters = [
+            'foo'      => 'ciao',
+            'bar'      => '0',
+            'baz'      => '',
+            'implicit' => '',
+        ];
+
+        $appliedFilters = [
+            'foo'      => 'ciao',
+            'bar'      => '0',
+            'implicit' => '',
+        ];
+
+        $request->all()->willReturn($filters);
+
+        $this->applyToQuery($query)->shouldReturn($query);
+
+        $this->filters->shouldBe($appliedFilters);
+    }
+
+    /**
+     * @testdox    It does not apply non existent filters.
+     *
+     * @author    Andrea Marco Sartori
+     * @return    void
+     */
+    public function it_does_not_apply_non_existent_filters($request, Builder $query)
+    {
+        $filters = [
+            'foo'          => 'ciao',
+            'bar'          => '0',
+            'non_existent' => '',
+            'foo-bar_baz'  => 'null',
+        ];
+
+        $appliedFilters = [
+            'foo'         => 'ciao',
+            'bar'         => '0',
+            'foo-bar_baz' => 'null',
+        ];
+
+        $request->all()->willReturn($filters);
+
+        $this->applyToQuery($query)->shouldReturn($query);
+
+        $this->filters->shouldBe($appliedFilters);
     }
 }
